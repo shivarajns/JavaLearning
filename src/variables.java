e6224c3b6e78a67c449b83aacdc2f6e916aaa0f2
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-07-18 22:25:54 +0530

    variables in java

---
+++ src/variables.java
@@ -0,0 +1,12 @@
+public class variables {
+    public static void main(String[] args) {
+
+        // variables declaration
+        String Name = "Shivaraju NS";
+        int Age = 21;
+        double Height = 5.11;
+        boolean isStudent = true;
+
+        System.out.println(Name + " is an " + Age + " year old boy " + "his height is " + Height + "and he is an student: " + isStudent);
+    }
+}
