c28286fb8317869fd6fa773ecbf94c5c72bfbe58
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-07-19 12:48:55 +0530

    substring Methods in Java.

---
+++ src/substring.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class substring {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
+        System.out.print("Enter your mail: ");
+        String email = scnr.nextLine();
+        String name = email.substring(0, email.indexOf("@"));
+        String domain=email.substring((email.indexOf("@")+1));
+
+        System.out.println("Your name is: " + name);
+        System.out.println("Your domain is: "+domain);
+    }
+}
