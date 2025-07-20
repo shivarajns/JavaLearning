6f7c13e1a5caf95038616e371ea6087ef4f39f77
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-07-20 19:45:45 +0530

    break and continue functions in java

---
+++ src/break_And_continue.java
@@ -0,0 +1,20 @@
+public class break_And_continue {
+    public static void main(String[] args) {
+        for(int i=0; i<=10; i++){
+            if(i==5){
+                break;
+            }
+            System.out.print(i+" ");
+        }
+
+            System.out.println(" ");
+        //continue
+
+        for(int i=0;i<=10;i++){
+            if(i==5){
+                continue;
+            }
+            System.out.print(i+" ");
+        }
+    }
+}
