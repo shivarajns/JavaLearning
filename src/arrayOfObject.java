98dc17fdb8ded01ca70a8cf726960387de2b32c2
Shivaraju NS <shivarajsmurthy@gmail.com>
2025-07-30 22:56:47 +0530

    Array of Objects in java

--- src/arrayOfObject.java
+++ src/arrayOfObject.java
@@ -1,7 +1,7 @@
 public class arrayOfObject {
     public static void main(String[] args) {
         arrayOfObjectClass cons1 = new arrayOfObjectClass("Innova", "White");
-        arrayOfObjectClass cons2 = new arrayOfObjectClass("Benz", "red");
+        arrayOfObjectClass cons2 = new arrayOfObjectClass("Benz", "Red");
 
         arrayOfObjectClass [ ] cars = {cons1, cons2};
         for(int i=0; i<cars.length; i++) {
@@ -12,6 +12,21 @@ public class arrayOfObject {
         System.out.println("********************************");
         System.out.println("Using Enhanced For Loop");
         for (arrayOfObjectClass car :cars){
+            car.drive();
+
+        }
+        System.out.println("******************************");
+        arrayOfObjectClass[] cars1 = { new arrayOfObjectClass("Swift", "blue"),
+                                       new arrayOfObjectClass("Nexon", "black")};
+
+        for (arrayOfObjectClass car : cars1) {
+            car.drive();
+        }
+        System.out.println("******************************");
+        for (arrayOfObjectClass car : cars1) {
+            car.color = "pink";
+        }
+        for (arrayOfObjectClass car : cars1) {
             car.drive();
         }
     }
